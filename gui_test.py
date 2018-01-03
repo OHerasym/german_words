e3c26420d634d454660344e6a43e8925b5b53980
Herasym <OHerasym@kiev.luxoft.com>
2018-01-03 17:10:43 +0200

    add Ukr-German

--- gui_test.py
+++ gui_test.py
@@ -14,11 +14,21 @@ for i in range(len(words)):
 	random_1 = random.randrange(len(words))
 	random_2 = random.randrange(len(words))
 	random_3 = random.randrange(len(words))
-	answers = [words[random_1][1], words[random_2][1], words[random_3][1]]
-	set_answers(answers)
-	answer = easygui.buttonbox(words[random_1][0], 'German', answers)
 
+	random_4 = random.randrange(len(words))
+	random_5 = random.randrange(len(words))
+	random_6 = random.randrange(len(words))
+
+	answers = [words[random_1][1], words[random_2][1], words[random_3][1]]
+	answers_2 = [words[random_4][0], words[random_5][0], words[random_6][0]]
+	set_answers(answers)
+	set_answers(answers_2)
+
+	answer = easygui.buttonbox(words[random_1][0], 'German', answers)
 	print('OK!') if answer is words[random_1][1] else print('Wrong!')
+
+	answer_2 = easygui.buttonbox(words[random_4][1], 'Ukr', answers_2)
+	print('OK2!') if answer_2 is words[random_4][0] else print('Wrong2!')
 	
 	field_names = ['Enter word:']
 	field_value = easygui.multenterbox(words[random_1][0], '123', field_names)
